71b2a8398a50f57f866608cf2097c44c3b4007b2
chasyuss <cotn963@gmail.com>
2025-07-14 23:47:17 +0900

    #3 [SL-44] feat: 푸터 구현

---
+++ src/common/Footer.tsx
@@ -0,0 +1,38 @@
+import Matching from '../assets/icons/MachingIcon.svg?react';
+import Search from '../assets/icons/Search.svg?react'
+import MapIcon from '../assets/icons/MapIcon.svg?react'
+import CommunityIcon from '../assets/icons/CommunityIcon.svg?react'
+import MyPageIcon from '../assets/icons/MyPageIcon.svg?react'
+
+const Footer = () => {
+  return (
+  <div className="flex w-full h-[56px] px-2 items-center bg-white justify-between flex-row">
+      <div className="flex w-[70px] flex-col justify-center items-center gap-1 cursor-pointer">
+        <Matching  />
+        <p className="text-[#B3B3B3] font-glyph11-R">매칭</p>
+      </div>
+
+      <div className="flex w-[70px] flex-col justify-center items-center gap-1 cursor-pointer">
+        <Search  />
+        <p className="text-[#B3B3B3] font-glyph11-R">검색</p>
+      </div>
+
+      <div className="flex w-[70px] flex-col justify-center items-center gap-1 cursor-pointer">
+        <MapIcon  />
+        <p className="text-[#B3B3B3] font-glyph11-R">지도</p>
+      </div>
+
+      <div className="flex w-[70px] flex-col justify-center items-center gap-1 cursor-pointer">
+        <CommunityIcon  />
+        <p className="text-[#B3B3B3] font-glyph11-R">커뮤니티</p>
+      </div>
+
+      <div className="flex w-[70px] flex-col justify-center items-center gap-1 cursor-pointer">
+        <MyPageIcon  />
+        <p className="text-[#B3B3B3] font-glyph11-R">마이페이지</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
