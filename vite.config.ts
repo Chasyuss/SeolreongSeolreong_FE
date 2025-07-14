a6b37c595e971d85b4c082bd3121e1c2c1f61015
chasyuss <cotn963@gmail.com>
2025-07-14 15:55:52 +0900

    #3 [SL-44] chore: svg 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // React 사용하는 경우만
+import react from "@vitejs/plugin-react";
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(), 
+  ],
 });
